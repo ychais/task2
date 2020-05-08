adfdd4e8a50d1e0887f0f70103cbc57dae8b0ba6
ychais <62468360+ychais@users.noreply.github.com>
2020-05-08 16:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, send_static_file
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
 def index():
     return render_template("index.html")
+ 
 
+@app.route('/static/<path:path>')
+def index2(path):
+    return app.send_static_file(path)
+
+ 
 if __name__ =="__main__":
     app.run(host='localhost', port=5000, debug=True)
