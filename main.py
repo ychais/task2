3fc9bc1dbb95c03c24f86a934b45a668213a555f
ychais <62468360+ychais@users.noreply.github.com>
2020-05-08 22:16:04 +0300

    Update main.py
    Changed routes for images and css file.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, send_static_file
-
+from flask import Flask, send_from_directory, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
- 
 
-@app.route('/static/<path:path>')
-def index2(path):
-    return app.send_static_file(path)
+@app.route('/static/images/<path:filename>')
+def index1 (filename):
+    return send_from_directory('images', filename)
 
- 
-if __name__ =='__main__':
+@app.route('/static/css/<path:filename>')
+def index2 (filename):
+    return send_from_directory('css', filename)
+
+if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
